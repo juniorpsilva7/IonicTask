d18c1e81d573fbb09ffaf1786e1cb300b582705c
Junior Silva <juniorpsilva7@gmail.com>
2016-11-04 00:16:44 -0200

    Aula 7 Hide/Show completed tasks

--- www/js/app.js
+++ www/js/app.js
@@ -28,10 +28,15 @@ app.controller('mainController', function($scope){
   var tasks = new getTasks();
 
   $scope.lista = tasks.items;
+  $scope.showMarked = false;
 
   $scope.onMarkTask = function(item){
     console.log("passou");
     item.finalizada  = !item.finalizada;
+  };
+
+  $scope.onShowItem = function(item){
+    return item.finalizada && !$scope.showMarked;
   };
 
 });
