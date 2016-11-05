e37454613c59c64cf43009a7902432aec048a78b
Junior Silva <juniorpsilva7@gmail.com>
2016-11-05 20:08:10 -0200

    Aula8 Add remove function

--- www/js/TasksModel.js
+++ www/js/TasksModel.js
@@ -4,4 +4,10 @@ function getTasks(){
         {nome:'Item 02', finalizada:false}, 
         {nome:'Item 03', finalizada:false}
         ];
+
+
+    this.remove = function(item){
+        var pos = this.items.indexOf(item);
+        this.items.splice(pos, 1);
+    };
 }
--- www/js/app.js
+++ www/js/app.js
@@ -29,6 +29,7 @@ app.controller('mainController', function($scope){
 
   $scope.lista = tasks.items;
   $scope.showMarked = false;
+  $scope.removeStatus = false;
 
   $scope.onMarkTask = function(item){
     console.log("passou");
@@ -38,5 +39,14 @@ app.controller('mainController', function($scope){
   $scope.onShowItem = function(item){
     return item.finalizada && !$scope.showMarked;
   };
+
+  $scope.onItemRemove = function(item){
+    tasks.remove(item);
+  };
+
+  $scope.onClickRemove = function(){
+    $scope.removeStatus = !$scope.removeStatus;
+  };
+
 
 });
